b31b86d768b57d308dde5c5db63162c774fbb3c3
root <root@node1.fsmt.cloudnet-pg0.utah.cloudlab.us>
2019-12-07 04:58:13 -0700

    added logging for fission/hello

--- fission/hello-function/hello-function.py
+++ fission/hello-function/hello-function.py
@@ -1,2 +1,12 @@
+from flask import current_app
+import logging
+import datetime
+
 def main():
+    logging.basicConfig(filename='measure.log',level=logging.DEBUG)
+    start_time = datetime.datetime.now()
+    finish_time = datetime.datetime.now()
+    current_app.logger.info('Invoke Started at: ' + str(start_time.time()))
+    current_app.logger.info('Invoke Finished at: ' + str(finish_time.time()))
+    current_app.logger.info('Invoke Duration: ' + str(finish_time-start_time))
     return "Hello, Fission!\n"
