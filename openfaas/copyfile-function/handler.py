6b4dc0b8991def7e97b5a7d178bd151df00331f5
bdani93 <36195643+bdani93@users.noreply.github.com>
2019-12-11 19:40:51 +0100

    Update handler.py

--- openfaas/copyfile-function/handler.py
+++ openfaas/copyfile-function/handler.py
@@ -6,6 +6,8 @@ import sys
 import os
 import curses
 
+logging.basicConfig(level=logging.INFO)
+
 def copy(srcfile, destfile):
     with open(srcfile) as source:
         with open(destfile, 'w') as destination:
@@ -19,15 +21,14 @@ def read(destfile):
 
 def handle(req):
     logging.basicConfig(filename='measure.log',level=logging.DEBUG)
-    logging.StreamHandler(sys.stderr)
     start_time = datetime.datetime.now()
     logging.info('Invoke Started at: ' + str(start_time.time()))
     current_dir = os.path.dirname(__file__)
-    srcfile = str(request.args.get('src'))
-    destfile = str(request.args.get('dest'))
+    srcfile = str(req)
+    destfile = "out.txt"
     copy(os.path.join(current_dir, srcfile), os.path.join(current_dir, destfile))
     ered = read(os.path.join(current_dir, destfile))
     finish_time = datetime.datetime.now()
-    current_app.logger.info('Invoke Finished at: ' + str(finish_time.time()))
-    current_app.logger.info('Invoke Duration: ' + str(finish_time-start_time))
+    logging.info('Invoke Finished at: ' + str(finish_time.time()))
+    logging.info('Invoke Duration: ' + str(finish_time-start_time))
     return ered
