a82f03045a66380294d820852ffa473887d76e74
bdani93 <36195643+bdani93@users.noreply.github.com>
2019-12-11 10:56:56 +0100

    Update handler.py

--- openfaas/copyfile-function/handler.py
+++ openfaas/copyfile-function/handler.py
@@ -1,9 +1,8 @@
-from flask import request
-from flask import current_app
 import logging
 import datetime
 import sys
 import os
+import curses
 
 def copy(srcfile, destfile):
     with open(srcfile) as source:
@@ -21,9 +20,10 @@ def handle(req):
     start_time = datetime.datetime.now()
     current_app.logger.info('Invoke Started at: ' + str(start_time.time()))
     current_dir = os.path.dirname(__file__)
-    srcfile = req
+    srcfile= str(req, 'utf-8')
+    destfile = "out.txt"
     copy(os.path.join(current_dir, srcfile), os.path.join(current_dir, destfile))
-    ered = read(os.path.join(current_dir, "out.txt"))
+    ered = read(os.path.join(current_dir, destfile))
     finish_time = datetime.datetime.now()
     current_app.logger.info('Invoke Finished at: ' + str(finish_time.time()))
     current_app.logger.info('Invoke Duration: ' + str(finish_time-start_time))
