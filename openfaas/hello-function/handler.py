80ae6b8e458cc330b52b793ba76b768c65ca9eb8
bdani93 <36195643+bdani93@users.noreply.github.com>
2019-12-11 23:34:41 +0100

    update openfaas/hello-function with logging

--- openfaas/hello-function/handler.py
+++ openfaas/hello-function/handler.py
@@ -1,2 +1,14 @@
+
+import logging
+import datetime
+
+logging.basicConfig(level=logging.INFO)
+
 def handle(req):
+    logging.basicConfig(filename='measure.log',level=logging.DEBUG)
+    start_time = datetime.datetime.now()
+    logging.info('Invoke Started at: ' + str(start_time.time()))
+    finish_time = datetime.datetime.now()
+    logging.info('Invoke Finished at: ' + str(finish_time.time()))
+    logging.info('Invoke Duration: ' + str(finish_time - start_time))
     print("Hello Openfaas!")
