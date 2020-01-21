ec519de6d79d80de466af1e61d6f917cde86d7e1
root <root@node1.bd-szd.cloudnet-pg0.utah.cloudlab.us>
2020-01-21 11:13:25 -0700

    randomize filename

--- fission/copy-function/copy-function.py
+++ fission/copy-function/copy-function.py
@@ -4,16 +4,23 @@ import logging
 import datetime
 import sys
 import os
+import uuid
+
+def read(destfile):
+    with open(destfile) as destination:
+        return destination.read()
+
 
 def main():
     logging.basicConfig(filename='measure.log',level=logging.DEBUG)
     start_time = datetime.datetime.now()
     current_app.logger.info('Invoke Started at: ' + str(start_time.time()))
     srcdata = request.args.get('src')
-	with open("Outfile.txt", 'w') as text_file:
-	   text_file.write("Out: %s" % srcdata)
-	   
+    destfile = str(uuid.uuid4())
+    with open(destfile, 'w') as text_file:
+        text_file.write("Out: %s" % srcdata)
+
     finish_time = datetime.datetime.now()
     current_app.logger.info('Invoke Finished at: ' + str(finish_time.time()))
     current_app.logger.info('Invoke Duration: ' + str((finish_time - start_time).microseconds))
-    return ered
+    return read(destfile) 
