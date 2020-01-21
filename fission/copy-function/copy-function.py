b29561bb6392df8fbb184033da32583e468e7abd
Bartok Daniel <bdani93@gmail.com>
2020-01-21 18:57:10 +0100

    fix copy-function

--- fission/copy-function/copy-function.py
+++ fission/copy-function/copy-function.py
@@ -4,17 +4,15 @@ import logging
 import datetime
 import sys
 import os
-import uuid
 
 def main():
     logging.basicConfig(filename='measure.log',level=logging.DEBUG)
     start_time = datetime.datetime.now()
     current_app.logger.info('Invoke Started at: ' + str(start_time.time()))
     srcdata = request.args.get('src')
-    destfile = str(uuid.uuid4())
-    current_dir = os.path.dirname(__file__)
-	with open(destfile, 'w') as text_file:
-	   print("{}".format(srcdata), file=text_file)
+	with open("Outfile.txt", 'w') as text_file:
+	   text_file.write("Out: %s" % srcdata)
+	   
     finish_time = datetime.datetime.now()
     current_app.logger.info('Invoke Finished at: ' + str(finish_time.time()))
     current_app.logger.info('Invoke Duration: ' + str((finish_time - start_time).microseconds))
